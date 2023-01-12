6cb01fe4c3f24b5e7bfa36ead4ee176445c9c8a4
Victor Chin <victor.chin@yahoo.com>
2023-01-12 09:03:11 -0800

    change Function1 message

--- GitHubActionsChallengeFunctionApp/Function1.cs
+++ GitHubActionsChallengeFunctionApp/Function1.cs
@@ -27,7 +27,7 @@ namespace GitHubActionsChallengeFunctionApp
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. How You Doing??";
 
             return new OkObjectResult(responseMessage);
         }
